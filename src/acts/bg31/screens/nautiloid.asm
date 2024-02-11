4b1d5ebfb3fc7dd46581dfaf80c7e9d53c45a39c
Alex Bowen <ajbowen249@gmail.com>
2024-02-11 14:26:57 -0500

    feat: more nautiloid wip

--- src/acts/bg31/screens/nautiloid.asm
+++ src/acts/bg31/screens/nautiloid.asm
@@ -3,14 +3,14 @@
 .local
 nautiloid_data:
 nautiloid_background:
-.asciz " ██◤   ◥████◤   ◥██ "
-.asciz " █◤     ◥██◤     ◥█ "
-.asciz " ◤       ◥◤       ◥ "
+.asciz "██◤   ◥█████◤   ◥██ "
+.asciz "█◤     ◥███◤     ◥█ "
+.asciz "◤       ◥█◤       ◥ "
 .asciz "                  ▐ "
 .asciz "                  ▐ "
-.asciz " ◣       ◢◣       ◢ "
-.asciz " █◣     ◢██◣     ◢█ "
-.asciz " ██◣   ◢████◣   ◢██ "
+.asciz "◣       ◢█◣       ◢ "
+.asciz "█◣     ◢███◣     ◢█ "
+.asciz "██◣   ◢█████◣   ◢██ "
 nautiloid_title: .asciz "Nautiloid"
 .db 0
 .db 0
@@ -26,8 +26,8 @@ nautiloid_title: .asciz "Nautiloid"
 nautiloid_start_x: .db 16 ; 1-indexed since it's screen coordinates!
 nautiloid_start_y: .db 4
 nautiloid_interactables:
-    DEFINE_INTERACTABLE door_1, in_door, $01, 4, 20
-    DEFINE_INTERACTABLE blank_2, 0, 0, 0, 0
+    DEFINE_INTERACTABLE door_1, in_door, $01, 4, 1
+    DEFINE_INTERACTABLE door_2, in_door, $01, 5, 1
     DEFINE_INTERACTABLE blank_3, 0, 0, 0, 0
     DEFINE_INTERACTABLE blank_4, 0, 0, 0, 0
     DEFINE_INTERACTABLE blank_5, 0, 0, 0, 0
@@ -35,7 +35,7 @@ nautiloid_interactables:
     DEFINE_INTERACTABLE blank_7, 0, 0, 0, 0
     DEFINE_INTERACTABLE blank_8, 0, 0, 0, 0
     DEFINE_INTERACTABLE blank_9, 0, 0, 0, 0
-    DEFINE_INTERACTABLE blank_0, 0, 0, 0, 0
+    DEFINE_INTERACTABLE blank_10, 0, 0, 0, 0
 nautiloid_get_interaction_prompt: .dw get_interaction_prompt
 nautiloid_interact_callback: .dw on_interact
 
@@ -54,6 +54,9 @@ get_interaction_prompt:
 
 on_interact:
     cp a, 0
+    jp z, door_interact
+
+    cp a, 1
     jp z, door_interact
     ret
 
