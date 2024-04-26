29bea54ae39932f8306ddd2c0a381051f0b11b0d
Alex Bowen <ajbowen249@gmail.com>
2024-04-25 21:39:37 -0400

    feat: add additional enemies to nere fight

--- src/acts/bg31/encounters/nere.asm
+++ src/acts/bg31/encounters/nere.asm
@@ -20,7 +20,41 @@ encounter_nere::
     ld a, pl_data_size
     call copy_hl_bc
 
-    ld a, 2
+    ld hl, thrinn_name
+    ld bc, enemy_player_2_name
+    ld a, 10
+    call copy_hl_bc
+
+    ld a, 12
+    ld (enemy_player_2_level), a
+
+    ld hl, monster_duergar
+    ld bc, enemy_player_3
+    ld a, pl_data_size
+    call copy_hl_bc
+
+    ld hl, brithvar_name
+    ld bc, enemy_player_3_name
+    ld a, 10
+    call copy_hl_bc
+
+    ld a, 11
+    ld (enemy_player_3_level), a
+
+    ld hl, monster_duergar
+    ld bc, enemy_player_4
+    ld a, pl_data_size
+    call copy_hl_bc
+
+    ld hl, dalthar_name
+    ld bc, enemy_player_4_name
+    ld a, 10
+    call copy_hl_bc
+
+    ld a, 10
+    ld (enemy_player_4_level), a
+
+    ld a, 4
     ld (enemy_party_size), a
 
     call start_encounter
